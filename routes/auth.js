55c25b7724e76be7854a27e75d76b257f82636c6
Savan <savan.nagariya@gmail.com>
2022-02-19 13:50:36 +0530

    mongo db database connected

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+router.get("/login");
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
