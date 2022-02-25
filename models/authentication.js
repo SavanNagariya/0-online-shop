010adb9c8d589a10ed9b299857c3fc64da9b8f1e
Savan <savan.nagariya@gmail.com>
2022-02-26 00:08:56 +0530

    auth modal added

---
+++ models/authentication.js
@@ -0,0 +1,28 @@
+const db = require("../data/database");
+const bcrypt = require("bcryptjs");
+class User {
+  constructor(userData) {
+    this.email = userData.email;
+    this.password = userData.password;
+    this.name = userData.name;
+    this.street = userData.street;
+    this.postalcode = userData.postalcode;
+    this.city = userData.city;
+  }
+
+  async signup() {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+
+    const users = {
+      email: this.email,
+      password: hashedPassword,
+      name: this.name,
+      street: this.street,
+      postalcode: this.postalcode,
+      city: this.city,
+    };
+    await db.getDb().collection("users").insertOne(users);
+  }
+}
+
+module.exports = User;
